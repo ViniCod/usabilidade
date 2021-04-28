8ce5ea8c1eb2d33b921528318493dc320a457455
ViniCod <viniciusprates.6798@aluno.saojudas.br>
2021-04-27 21:57:04 -0300

    Aula 2020-04-27

--- mean/src/app/app.module.ts
+++ mean/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppComponent } from './app.component';
 import { ClienteInserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { ClienteListaComponent } from './clientes/cliente-lista/cliente-lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClienteInserirComponent
+    ClienteInserirComponent,
+    CabecalhoComponent,
+    ClienteListaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- mean/src/app/clientes/cliente-inserir/cliente-inserir.component.ts
+++ mean/src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 
 @Component ({
   selector: 'app-cliente-inserir',
-  templateUrl: './cliente-inserir.component.html'
+  templateUrl: './cliente-inserir.component.html',
+  styleUrls: ['./cliente-inserir.component.css'],
 })
 
 export class ClienteInserirComponent {
+
+  nome: string;
+  telefone: string;
+  email: string;
+
   onAdicionarCliente() {
     console.log('Inserindo cliente');
   }
